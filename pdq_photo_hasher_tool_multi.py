253991e8e54a34114008d6613641f159f26b479e
Morgan Guesdon <270189+momo286@users.noreply.github.com>
2024-09-08 14:55:04 +0200

    Update pdq_photo_hasher_tool_multi.py

--- pdq_photo_hasher_tool_multi.py
+++ pdq_photo_hasher_tool_multi.py
@@ -6,15 +6,12 @@ import multiprocessing
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 from pdqhashing.hasher.pdq_hasher import PDQHasher
 
-
 def hash_file(path):
     pdq_hasher = PDQHasher()
     try:
         hash_and_quality = pdq_hasher.fromFile(path)
         hash_code = hash_and_quality.getHash()  
         result = f"{str(hash_code)},{path}"
-    except IOError as e:
-        result = f"Error reading {path}: {e}"
     except Exception as e:
         result = f"Error processing {path}: {e}"
     return result
@@ -29,14 +26,10 @@ def compute_pdq_hashes(directory, output_hashes, num_processes):
 def main():
     parser = argparse.ArgumentParser(description="Compute PDQ hashes for image files.")
     parser.add_argument("directory", help="Directory to recursively process.")
-    parser.add_argument("--output", type=argparse.FileType('w'), default=sys.stdout,
-                        help="Output file to write the hashes to.")
     parser.add_argument("--num-processes", type=int, default=(multiprocessing.cpu_count()),
                         help="Number of processes to use for hashing.")
     args = parser.parse_args()
-
     dir=args.directory
-
     compute_pdq_hashes(args.directory, args.output, args.num_processes)
 
 if __name__ == "__main__":
